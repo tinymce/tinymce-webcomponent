c9a911969e65f29dff7a7fa3273ecd83a0d42c05
a.dmitryuk <a.dmitryuk@movavi.com>
2025-12-16 10:11:06 +0700

    add tests

--- src/test/ts/browser/LoadTest.ts
+++ src/test/ts/browser/LoadTest.ts
@@ -33,13 +33,18 @@ UnitTest.asynctest('LoadTest', (success, failure) => {
     }),
     Step.sync(() => makeTinymceElement({
       'setup': 'customElementTinymceSetup',
-      'on-init': 'customElementTinymceInit'
+      'on-init': 'customElementTinymceInit',
+      'id': 'example_id'
     }, '<p>Hello world</p>')),
     Waiter.sTryUntilPredicate('Waiting for editor setup', () => seenSetup),
     Waiter.sTryUntilPredicate('Waiting for editor init', () => seenInit),
     Step.sync(() => {
       Assertions.assertHtmlStructure('', '<p>Hello world</p>', editorInstance.getContent() as string);
+      Assertions.assertEq('Global TinyMCE is not set', true, Global.tinymce.get('example_id') !== null);
     }),
-    Step.sync(() => removeTinymceElement())
+    Step.sync(() => removeTinymceElement()),
+    Step.sync(() => {
+      Assertions.assertEq('Global TinyMCE not destroyed', true, Global.tinymce.get('example_id') === null);
+    })
   ], success, failure);
 });
