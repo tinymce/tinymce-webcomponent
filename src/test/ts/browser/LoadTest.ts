c8b9043671ec8c025afddb59ad698c35e805f86e
Ben Tran <ben.tran@tiny.cloud>
2026-04-07 15:03:03 +0930

    Another go

--- src/test/ts/browser/LoadTest.ts
+++ src/test/ts/browser/LoadTest.ts
@@ -3,19 +3,27 @@ import { before, describe, after, it } from '@ephox/bedrock-client';
 import { Global } from '@ephox/katamari';
 import { createTinymceElement, deleteTinymce, registerCustomElementIfNot, removeTinymceElement } from '../alien/Utils';
 import { VersionLoader } from '@tinymce/miniature';
+import { Editor } from 'tinymce';
 
 describe('LoadTest', () => {
-  let seenSetup = false;
-  let seenInit = false;
-  let editorInstance: any;
-
   before(async () => {
     await VersionLoader.pLoadVersion('8');
     registerCustomElementIfNot();
     Global.tinymceTestConfig = { license_key: 'gpl' };
+  });
+
+  after(() => {
+    delete Global.tinymceTestConfig;
+    deleteTinymce();
+  });
+
+  it('Should load the editor and execute setup and init callbacks', async () => {
+    let seenSetup = false;
+    let seenInit = false;
+    let editorInstance: Editor | undefined;
 
     await new Promise((resolve) => {
-      Global.customElementTinymceSetup = (editor: any) => {
+      Global.customElementTinymceSetup = (editor: Editor) => {
         seenSetup = true;
         editorInstance = editor;
       };
@@ -30,18 +38,13 @@ describe('LoadTest', () => {
         'id': 'example_id'
       }, '<p>Hello world</p>');
     });
-  });
 
-  after(() => {
-    delete Global.tinymceTestConfig;
-    removeTinymceElement();
-    deleteTinymce();
-  });
-
-  it('Should load the editor and execute setup and init callbacks', () => {
     Assertions.assertEq('Editor setup callback should be called', true, seenSetup);
     Assertions.assertEq('Editor init callback should be called', true, seenInit);
     Assertions.assertEq('An editor instance is registered', true, Global.tinymce.get('example_id') !== null);
-    Assertions.assertHtmlStructure('The editor has the correct content', '<p>Hello world</p>', editorInstance.getContent() as string);
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    Assertions.assertHtmlStructure('The editor has the correct content', '<p>Hello world</p>', editorInstance!.getContent() as string);
+
+    removeTinymceElement();
   });
 });
